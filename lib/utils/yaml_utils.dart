00ccf318f71eabcc65270dba41f6aae7e4cdd139
jose.ugarte <jose@5pire.com>
2023-03-28 23:48:23 +0100

    Remove flutter services dependencies

--- lib/utils/yaml_utils.dart
+++ lib/utils/yaml_utils.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
+import "file_utils.dart";
 import 'package:yaml/yaml.dart' show loadYaml;
 import 'dart:developer';
 
 Future<Map<String, dynamic>> loadYamlAsset(String path) async {
-  final yamlString = await rootBundle.loadString(path);
+  final yamlString = await readAsync(path);
   final yamlMap = loadYaml(yamlString);
   print("yamlMap.keys: ${yamlMap.keys}");
   var result = Map<String, dynamic>.from(yamlMap);
